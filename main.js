bb17e0f67026cf0b632ffaa2e135eb0d331a1133
L3V!N <85253400+levin1499@users.noreply.github.com>
2023-09-08 12:47:08 +0300

    js update

--- main.js
+++ main.js
@@ -24,3 +24,20 @@ var swiper = new Swiper(".home-slider", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+let imgBx = document.querySelectorAll(".imgBx");
+let contentBx = document.querySelectorAll(".contentBx");
+
+for (let i = 0; i < imgBx.length; i++) {
+  imgBx[i].addEventListener("mouseover", function () {
+    for (let i = 0; i < contentBx.length; i++) {
+      contentBx[i].className = "contentBx";
+    }
+    document.getElementById(this.dataset.id).className = "contentBx active";
+
+    for (let i = 0; i < imgBx.length; i++) {
+      imgBx[i].className = "imgBx";
+    }
+    this.className = "imgBx active";
+  });
+}
